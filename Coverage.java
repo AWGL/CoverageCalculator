47d539400625fd213f2539f1f8476446bd5ecf6d
mcgml <mcgml@cardiff.ac.uk>
2016-01-08 10:52:05 +0000

    rewrote for GTF functionality

--- Coverage.java
+++ Coverage.java
@@ -1,16 +1,8 @@
 package nhs.genetics.cardiff;
 
-import htsjdk.tribble.AbstractFeatureReader;
-import htsjdk.tribble.bed.BEDCodec;
-import htsjdk.tribble.bed.BEDFeature;
-
-import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Created by matt on 10/03/15.
@@ -18,210 +10,36 @@ import java.util.logging.Logger;
 
 public class Coverage {
 
-    private static final Logger log = Logger.getLogger(Coverage.class.getName());
+    private int minDepth;
+    private HashSet<GenomicLocation> targetGenomicLocations;
+    private HashMap<GenomicLocation, Integer> depthOfCoverage;
+    private ArrayList<GenomicLocation> missingBases = new ArrayList<>();
+    private int passedBases = 0;
 
-    private int threshold;
-    private File coverageFile, bedFile;
-
-    private ArrayList<String> sampleIds = new ArrayList<>();
-
-    private HashMap<String, HashSet<GenomicLocation>> targetBasesByGene = new HashMap<>(); //Gene:[single-base pos]
-    private HashSet<GenomicLocation> totalTargetBases = new HashSet<>(); //[all single base pos]
-
-    private ArrayList<HashSet<GenomicLocation>> totalPassingBasesBySample = new ArrayList<>();
-    private ArrayList<ArrayList<GenomicLocation>> totalMissingBasesBySample = new ArrayList<>();
-    private ArrayList<HashMap<String, ArrayList<GenomicLocation>>> geneMissingBasesBySample = new ArrayList<>();
-
-    private ArrayList<ArrayList<GenomicLocation>> totalMissingRegionsBySample = new ArrayList<>();
-
-    public Coverage(File coverageFile, File bedFile, int threshold){
-        this.coverageFile = coverageFile;
-        this.bedFile = bedFile;
-        this.threshold = threshold;
+    public Coverage(HashSet<GenomicLocation> targetGenomicLocations, HashMap<GenomicLocation, Integer> depthOfCoverage, int minDepth){
+        this.minDepth = minDepth;
+        this.targetGenomicLocations = targetGenomicLocations;
+        this.depthOfCoverage = depthOfCoverage;
     }
-    public void populateTargetBases() {
 
-        log.log(Level.INFO, "Extracting target bases");
-
-        //read target bases into memory
-        try (AbstractFeatureReader reader = AbstractFeatureReader.getFeatureReader(bedFile.toString(), new BEDCodec(BEDCodec.StartOffset.ONE), false)) {
-            Iterable<BEDFeature> iter = reader.iterator();
-
-            for (BEDFeature feature : iter) {
-
-                String[] fields = feature.getName().split(":");
-
-                if (!targetBasesByGene.containsKey(fields[0])) targetBasesByGene.put(fields[0], new HashSet<GenomicLocation>());
-
-                //split bed feature into single base coordinates
-                for (int j = feature.getStart(); j < feature.getEnd() + 1; ++j) { //convert to single base 1-pos coordinates
-                    GenomicLocation genomicLocation = new GenomicLocation(feature.getContig(), j);
-
-                    //store unique bases from a single region
-                    targetBasesByGene.get(fields[0]).add(genomicLocation);
-
-                    //store all target bases
-                    totalTargetBases.add(genomicLocation);
-                }
-
+    public void populateCoverageMetics(){
+        for (GenomicLocation genomicLocation : targetGenomicLocations){
+            if (depthOfCoverage.containsKey(genomicLocation) && depthOfCoverage.get(genomicLocation) >= minDepth){
+                passedBases++;
+            } else {
+                missingBases.add(genomicLocation);
             }
-
-            reader.close();
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.toString());
         }
-
     }
-    public void extractBasesPassingMinDepth(){
 
-        log.log(Level.INFO, "Extracting bases passing minimum depth");
-
-        boolean passHeaderLine = false;
-        String line;
-
-        //read gatk per-base depth file; store bases passing QC
-        try (BufferedReader reader = new BufferedReader(new FileReader(coverageFile))){
-
-            while ((line = reader.readLine()) != null) {
-
-                if (!line.equals("")) {
-                    String[] fields = line.split("\t");
-
-                    //store SampleIDs
-                    if (!passHeaderLine){
-
-                        for (int n = 3 ; n < fields.length; ++n){
-
-                            //store sampleIDs
-                            String[] subFields = fields[n].split("_");
-                            sampleIds.add(subFields[2]);
-
-                            //initalise array
-                            totalPassingBasesBySample.add(new HashSet<GenomicLocation>());
-                            totalMissingBasesBySample.add(new ArrayList<GenomicLocation>());
-                            geneMissingBasesBySample.add(new HashMap<String, ArrayList<GenomicLocation>>());
-
-                        }
-
-                        passHeaderLine = true;
-
-                    } else {
-
-                        String[] subFields = fields[0].split(":");
-                        GenomicLocation genomicLocation = new GenomicLocation(subFields[0], Integer.parseInt(subFields[1]));
-
-                        for (int n = 3; n < fields.length; ++n) {
-
-                            //base passes minDP
-                            if (Integer.parseInt(fields[n]) >= threshold) {
-                                totalPassingBasesBySample.get(n - 3).add(genomicLocation);
-                            }
-                        }
-
-                    }
-
-                }
-
-            }
-
-            reader.close();
-        } catch (IOException e) {
-            log.log(Level.SEVERE, "Problem reading gatk depth file: " + e.getMessage());
-        }
-
+    public ArrayList<GenomicLocation> getWindowMissingBases() {
+        return GenomicLocation.mergeBases(missingBases);
     }
-    public void extractBasesFailingMinDepth(){ //todo optimise
-
-        log.log(Level.INFO, "Extracting bases failing minimum depth");
-
-        //Store bases failing QC wrt to targets
-        for (int n = 0; n < sampleIds.size(); ++n){
-
-            //total targets
-            for (GenomicLocation genomicLocation : totalTargetBases){
-                if (!totalPassingBasesBySample.get(n).contains(genomicLocation)){ //check if base passes QC
-                    totalMissingBasesBySample.get(n).add(genomicLocation);
-                }
-            }
-
-            //gene targets
-            for (Map.Entry<String, HashSet<GenomicLocation>> geneSet : targetBasesByGene.entrySet()){
-
-                //initalise
-                geneMissingBasesBySample.get(n).put(geneSet.getKey(), new ArrayList<GenomicLocation>());
-
-                for (GenomicLocation genomicLocation : geneSet.getValue()){
-
-                    if (!totalPassingBasesBySample.get(n).contains(genomicLocation)){ //check if base passes QC
-                        geneMissingBasesBySample.get(n).get(geneSet.getKey()).add(genomicLocation);
-                    }
-
-                }
-            }
-
-        }
-
+    public int getPassedBases() {
+        return passedBases;
     }
-    public void windowMissingBases() {
-
-        log.log(Level.INFO, "Converting single base coordinates to regions");
-
-        //Sort failed bases by chromosomal coordinate and condense into regions
-        for (int n = 0; n < sampleIds.size(); ++n) {
-            totalMissingRegionsBySample.add(GenomicLocation.mergeBases(totalMissingBasesBySample.get(n)));
-        }
-
-    }
-    public void printCoverageMetrics(){
-
-        log.log(Level.INFO, "Printing coverage metrics");
-
-        for (int n = 0; n < sampleIds.size(); ++n){
-
-            //print gaps
-            try (PrintWriter writer = new PrintWriter(sampleIds.get(n) + "_Gaps.bed")){
-
-                for (GenomicLocation genomicLocation : totalMissingRegionsBySample.get(n)){
-                    writer.println(genomicLocation.getContig() + "\t" + (genomicLocation.getStartPosition() - 1) + "\t" + genomicLocation.getEndPosition()); //convert to BED 0-based
-                }
-
-                writer.close();
-            } catch (IOException e){
-                log.log(Level.SEVERE, "Could not write to file: " + e.getMessage());
-            }
-
-        }
-
-        //print percentage coverage
-        try (PrintWriter writer = new PrintWriter("PercentageCoverage.txt")){
-
-            //print headers
-            writer.print("SampleId");
-
-            for (Map.Entry<String, HashSet<GenomicLocation>> geneSet : targetBasesByGene.entrySet()){
-                writer.print("\t" + geneSet.getKey());
-            }
-
-            writer.println();
-
-            //print % coverage by Gene
-            for (int n = 0; n < sampleIds.size(); ++n){
-
-                //print metrics
-                writer.print(sampleIds.get(n));
-
-                for (Map.Entry<String, HashSet<GenomicLocation>> geneSet : targetBasesByGene.entrySet()){
-                    writer.print("\t" + String.format("%.4g", ((double) (geneSet.getValue().size() - geneMissingBasesBySample.get(n).get(geneSet.getKey()).size()) / geneSet.getValue().size()) * 100));
-                }
-
-                writer.println();
-            }
-
-            writer.close();
-        } catch (IOException e) {
-            log.log(Level.SEVERE, "Could not write to percentage coverage file: " + e.getMessage());
-        }
-
+    public double getCoveragePercentage(){
+        return (double) passedBases / targetGenomicLocations.size();
     }
 
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 package nhs.genetics.cardiff;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -9,19 +15,114 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int minDepth = 30;
+        int padding = 20;
+
         if (args.length != 3){
-            System.err.println("Usage: <GATKCoverageFile> <BED> <Min>");
+            System.err.println("Usage: <PerBaseCoverage> <Gene/ENSTList> <GTF>");
+            System.err.println("-d Minimum depth [" + minDepth + "]");
+            System.err.println("-p Interval padding [" + padding + "]");
             System.exit(1);
         }
 
-        //calculate coverage from GATK depth of coverage
-        Coverage coverage = new Coverage(new File(args[0]), new File(args[1]), Integer.parseInt(args[2]));
+        String line, id;
+        HashMap<String, HashSet<GenomicLocation>> targetGenomicLocations = new HashMap<>();
 
-        coverage.populateTargetBases();
-        coverage.extractBasesPassingMinDepth();
-        coverage.extractBasesFailingMinDepth();
-        coverage.windowMissingBases();
-        coverage.printCoverageMetrics();
+        //parse gene list
+        ListReader listReader = new ListReader(new File(args[1]));
+        listReader.parseListReader();
+
+        //initalise map
+        for (String element : listReader.getUniqueElements()){
+            targetGenomicLocations.put(element, new HashSet<GenomicLocation>());
+        }
+
+        //create target list of bases; read GTF
+        log.log(Level.INFO, "Extracting target regions of interest");
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(args[2])))){
+            while ((line = reader.readLine()) != null) {
+
+                //skip headers
+                if (!Pattern.matches("^#.*", line))
+                {
+                    //split record and add to array
+                    GTFRecord gtfRecord = new GTFRecord(line);
+                    gtfRecord.parseGTFRecord();
+
+                    // ROI
+                    if ((gtfRecord.getFeature().equals("CDS") || gtfRecord.getFeature().equals("stop_codon")) && gtfRecord.getAttributes().get("transcript_biotype").equals("protein_coding")){
+
+                        if (listReader.getUniqueElements().contains(gtfRecord.getAttributes().get("gene_name"))){
+                            id = gtfRecord.getAttributes().get("gene_name");
+                        } else if (listReader.getUniqueElements().contains(gtfRecord.getAttributes().get("transcript_id"))){
+                            id = gtfRecord.getAttributes().get("transcript_id");
+                        } else {
+                            continue;
+                        }
+
+                        //split regions feature into single base coordinates
+                        for (int j = gtfRecord.getGenomicLocation().getStartPosition() - padding; j < gtfRecord.getGenomicLocation().getEndPosition() + padding; ++j) {
+                            targetGenomicLocations.get(id).add(new GenomicLocation(gtfRecord.getGenomicLocation().getContig(), j));
+                        }
+
+                    }
+
+                }
+
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "Could not read GTF file: " + e.getMessage());
+        }
+
+        //print missing ids
+        for (String element : listReader.getUniqueElements()){
+            if (targetGenomicLocations.get(element).size() == 0){
+                log.log(Level.WARNING, "Could not find: " + element);
+            }
+        }
+
+        //print ROI for debugging
+        try (PrintWriter printWriter = new PrintWriter("debug.bed")) {
+            for (GenomicLocation location : GenomicLocation.mergeBases(new ArrayList<>(targetGenomicLocations.get("BRCA1")))){
+                printWriter.println(location.getContig() + "\t" + (location.getStartPosition() - 1) + "\t" + location.getEndPosition());
+            }
+        } catch (IOException e){
+            log.log(Level.SEVERE, "could not write debug file");
+        }
+
+        //read GATK depth of coverage file
+        GatkDepthOfCoverageParser gatkDepthOfCoverageParser = new GatkDepthOfCoverageParser(new File(args[0]));
+        gatkDepthOfCoverageParser.parseGatkDepthOfCoverageFile();
+
+        //loop over samples and report coverage metrics
+        for (int n = 0; n < gatkDepthOfCoverageParser.getSampleIds().size(); ++n){
+            log.log(Level.INFO, "Generating coverage metrics for sample: " + gatkDepthOfCoverageParser.getSampleIds().get(n));
+
+            try (PrintWriter printWriter = new PrintWriter(gatkDepthOfCoverageParser.getSampleIds().get(n) + "_gaps.bed")) {
+
+                for (Map.Entry<String, HashSet<GenomicLocation>> target : targetGenomicLocations.entrySet()){
+                    log.log(Level.INFO, "Generating coverage metrics for target: " + target.getKey());
+
+                    //calculate coverage from per base coverage file
+                    Coverage coverage = new Coverage(target.getValue(), gatkDepthOfCoverageParser.getDepthOfCoverage().get(n), minDepth);
+                    coverage.populateCoverageMetics();
+
+                    System.out.println(gatkDepthOfCoverageParser.getSampleIds().get(n) + "\t" + target.getKey() + "\t" + coverage.getCoveragePercentage());
+
+                    for (GenomicLocation location : coverage.getWindowMissingBases()){
+                        printWriter.println(location.getContig() + "\t" + location.getStartPosition() + "\t" + location.getEndPosition());
+                    }
+
+                }
+
+            } catch (IOException e){
+                log.log(Level.SEVERE, "Could not write gaps for: " + gatkDepthOfCoverageParser.getSampleIds().get(n));
+            }
+
+        }
 
     }
 }
